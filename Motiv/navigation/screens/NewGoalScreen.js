9e1386f71604b171ba06cfeb924759cac2447677
Sinha, Eashan <esinha6@gatech.edu>
2022-12-07 10:11:49 -0500

    more notis

--- Motiv/navigation/screens/NewGoalScreen.js
+++ Motiv/navigation/screens/NewGoalScreen.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react'
 import { View, Text, Button } from 'react-native'
 import AddNewGoalForm from '../../components/AddNewGoalForm';
+
+
 function NewGoalScreen(props) {
     return <AddNewGoalForm navigation={props.navigation} setGoals={props.route.params.setGoals} setGoalsJson={props.route.params.setGoalsJson}  goalsJson={props.route.params.goalsJson} ></AddNewGoalForm>
 }
